a926cefad5ba4b4495c4c0212b5f832885541894
Marwan Kamil <mkamil001@st.ug.edu.gh>
2022-10-13 16:10:11 -0700

    printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
-/**
- * main-block
- * Return: 0
- **/
-int main(void)
-{
-	puts(“programming is like building a multilingual puzzle”\n)
-		return (0);
-}
+#include <stdio.h> 
+  
+ /** 
+  * main - Entry point 
+  * 
+  * Return: Always 0 (Success) 
+  */ 
+ int main(void) 
+ { 
+         puts("\"Programming is like building a multilingual puzzle"); 
+         return (0); 
+ }
