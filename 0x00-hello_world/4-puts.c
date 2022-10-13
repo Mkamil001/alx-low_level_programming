a26f90aec3cabfee29bcf7f47a7a25816cfee1c1
Marwan Kamil <mkamil001@st.ug.edu.gh>
2022-10-13 16:02:23 -0700

    printing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,6 @@
  **/
 int main(void)
 {
-	puts(““\”programming is like building a multilingual puzzle”);
-	return (0);
-
+	puts(“programming is like building a multilingual puzzle”\n)
+		return (0);
 }
