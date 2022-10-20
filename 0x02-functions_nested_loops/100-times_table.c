03f8c97dc3acb0940225a8b39b37a2fac19274a0
Marwan Kamil <mkamil001@st.ug.edu.gh>
2022-10-19 22:46:42 -0700

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "stdio.h"
 /**
  * print_times_table - Entry point
  * @n: input
@@ -6,6 +7,9 @@
  */
 void print_times_table(int n)
 {
+	if (n>15 | n<0)
+	{
 
 	return (0);
+	}
 }
